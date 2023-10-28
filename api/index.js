602f82b8eba63c91c0a773182e078dd8316c3ab0
SAURABH <saurabhdwivedi2310@gmail.com>
2023-10-28 20:32:42 +0530

    Connected to DATABASE

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
- const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGOURI).then(() => {
+   console.log('Connected to MongoDB !');
+ })
+ .catch((err) => {
+   console.log(err);
+ });
+
+const app = express();
+
 
  app.listen(5000 , () => {
     console.log(`Server is listening at port : 5000`);
  })
+
