71b0e973afd73ba8587813767d6af8b409158b8f
SAURABH <saurabhdwivedi2310@gmail.com>
2023-10-31 22:29:55 +0530

    Created and Updated Google Authentication

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { google, google, signin, signup } from '../controllers/auth.controller.js';
+import { google,signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
