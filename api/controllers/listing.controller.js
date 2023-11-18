95901344d7072bd979fbbe4c2d42179c1e50cd9a
SAURABH <saurabhdwivedi2310@gmail.com>
2023-11-18 22:05:41 +0530

    created API routes for update listing

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,13 +1,14 @@
 import Listing from '../models/listing.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
   } catch (error) {
-    next(error);
+    next(error); 
   }
-}; 
+};
 
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
@@ -27,4 +28,24 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
- 
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found!'));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, 'You can only update your own listings!'));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
