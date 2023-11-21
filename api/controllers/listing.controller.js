c9598cfa00cddb5e3617292ddf90deb1f6551455
SAURABH <saurabhdwivedi2310@gmail.com>
2023-11-21 22:12:52 +0530

    Completed UI of Update Listing unctionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -6,7 +6,7 @@ export const createListing = async (req, res, next) => {
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
   } catch (error) {
-    next(error); 
+    next(error);
   }
 };
 
@@ -49,3 +49,16 @@ export const updateListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListing = async (req, res, next) => {
+try {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found!'));
+  }
+  res.status(200).json(listing);
+} catch (error) {
+  next(error);
+}
+
+}
