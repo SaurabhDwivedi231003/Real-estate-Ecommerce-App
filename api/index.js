08be81ff0b794cbdfffa868e0fccacf52b2669eb
SAURABH <saurabhdwivedi2310@gmail.com>
2023-12-25 21:41:20 +0530

    changes for azure deployment

--- api/index.js
+++ api/index.js
@@ -36,6 +36,7 @@ app.use('/api/listing', listingRouter);
 
 // Static files and fallback route
 app.use(express.static(path.join(__dirname, '/client/dist')));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
